a7877ca185b505ed0614ee05acd031bc33f0f596
sisi florensia <sisiflorensia@gmail.com>
2019-10-25 00:22:07 +0800

    Css in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
